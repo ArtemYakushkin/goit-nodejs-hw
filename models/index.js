a8d8932820bfe34592046d120726f06bc64c397e
ArtemYakushkin <zimberger29101985@gmail.com>
2022-11-06 17:34:26 +0200

    add files filePath.js and index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const listContacts = require('./contacts');
+const getContactById = require('./contacts');
+const addContact = require('./contacts');
+const removeContact = require('./contacts');
+const updateContact = require('./contacts');
+
+module.exports = {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+    updateContact
+};
