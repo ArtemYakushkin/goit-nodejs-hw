1c0743fb11d948f85b10dd5b30349afec0e05267
ArtemYakushkin <zimberger29101985@gmail.com>
2022-11-13 17:31:09 +0200

    add validation

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -6,6 +6,15 @@ async function getAll(req, res, next) {
     return res.json({ data: contacts });
 };
 
+async function getById(req, res, next) { 
+    const { contactId } = req.params;
+    const result = await Contact.findById(contactId);
+    if (!result) {
+        throw createError(404, "Not found");
+    }
+    res.json(result);
+};
+
 async function add(req, res, next) {
     const newContact = await Contact.create({...req.body});
     return res.status(201).json({ data: { contact: newContact } });
@@ -22,8 +31,29 @@ async function removeById(req, res, next) {
     });
 };
 
+async function updateById(req, res, next) {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+    if (!result) {
+        throw createError(404, "Not found");
+    }
+    res.status(200).json(result);
+};
+ 
+async function updateStatusContact(req, res, next) {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+    if (!result) {
+        throw createError(404, "Not found");
+    }
+    res.status(200).json(result);
+};
+
 module.exports = {
     getAll,
+    getById,
     add,
     removeById,
+    updateById,
+    updateStatusContact
 };
--- routes/api/contacts.routes.js
+++ routes/api/contacts.routes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { ctrlWrapper } = require("../../helpers/ctrlWrapper");
 const contactsOperations = require("../../controllers/contacts.controller");
+const { validation } = require("../../middlewares/validation");
+const { addSchema, schemaUpdate, schemaUpdateFavorite } = require("../../models/contact.models");
 
 router.get('/', ctrlWrapper(contactsOperations.getAll));
-router.post('/', ctrlWrapper(contactsOperations.add));
+router.get("/:contactId", ctrlWrapper(contactsOperations.getById));
+router.post('/', validation(addSchema), ctrlWrapper(contactsOperations.add));
 router.delete('/:contactId', ctrlWrapper(contactsOperations.removeById));
+router.put('/:contactId', validation(schemaUpdate), ctrlWrapper(contactsOperations.updateById));
+router.patch('/:contactId', validation(schemaUpdateFavorite), ctrlWrapper(contactsOperations.updateStatusContact));
 
 
 module.exports = router;
